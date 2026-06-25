c47f2cb27c0ca0c783375a08ce578c0c85235f91
januwA <ajanuw1995@gmail.com>
2026-06-25 19:55:12 +0800

    feat: 添加 log, notification, store, window-state 插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -315,6 +315,10 @@ pub fn run() {
     }));
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::default().build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
