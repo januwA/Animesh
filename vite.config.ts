1301562251ad2cf52a991644323fa128ba4649ca
januwA <ajanuw1995@gmail.com>
2026-07-05 19:23:11 +0800

    打印环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -9,60 +9,64 @@ import { defineConfig } from "vite";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
-	plugins: [react(), tailwindcss()],
-	envPrefix: ["VITE_", "TAURI_ENV_*"],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig((config) => {
+	console.log(config,process.env);
+	
+	return ({
+		plugins: [react(), tailwindcss()],
+		envPrefix: ["VITE_", "TAURI_ENV_*"],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
 
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent Vite from obscuring rust errors
-	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
-	server: {
-		port: 1420,
-		strictPort: true,
-		host: host || false,
-		hmr: host
-			? {
+		// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+		//
+		// 1. prevent Vite from obscuring rust errors
+		clearScreen: false,
+		// 2. tauri expects a fixed port, fail if that port is not available
+		server: {
+			port: 1420,
+			strictPort: true,
+			host: host || false,
+			hmr: host
+				? {
 					protocol: "ws",
 					host,
 					port: 1421,
 				}
-			: undefined,
-		watch: {
-			// 3. tell Vite to ignore watching `src-tauri`
-			ignored: ["**/src-tauri/**"],
-		},
-	},
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/test/setup.ts",
-		coverage: {
-			provider: "v8",
-			reporter: ["text"],
-			include: ["src/**/*.{ts,tsx}"],
-			exclude: [
-				"src/main.tsx",
-				"src/test/**",
-				"**/*.test.{ts,tsx}",
-				"**/*.d.ts",
-				"src/components/ui/**",
-				"src/di/**",
-				"src/domain/**",
-				"src/infrastructure/**",
-			],
-			thresholds: {
-				lines: 100,
-				functions: 100,
-				branches: 95,
-				statements: 95,
+				: undefined,
+			watch: {
+				// 3. tell Vite to ignore watching `src-tauri`
+				ignored: ["**/src-tauri/**"],
 			},
 		},
-	},
-}));
+		test: {
+			globals: true,
+			environment: "jsdom",
+			setupFiles: "./src/test/setup.ts",
+			coverage: {
+				provider: "v8",
+				reporter: ["text"],
+				include: ["src/**/*.{ts,tsx}"],
+				exclude: [
+					"src/main.tsx",
+					"src/test/**",
+					"**/*.test.{ts,tsx}",
+					"**/*.d.ts",
+					"src/components/ui/**",
+					"src/di/**",
+					"src/domain/**",
+					"src/infrastructure/**",
+				],
+				thresholds: {
+					lines: 100,
+					functions: 100,
+					branches: 95,
+					statements: 95,
+				},
+			},
+		},
+	});
+});
